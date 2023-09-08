c9124abb5d0f1f9f37611ee71e8d4fe33cc96159
dalibor <dlipovac012@gmail.com>
2023-09-09 01:01:36 +0200

    Initial options

---
+++ init.lua
@@ -0,0 +1,2 @@
+require "prod.options"
+
---
+++ lua/prod/options.lua
@@ -0,0 +1,15 @@
+-- :help options
+
+
+-- System settings
+
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.writebackup = false
+
+
+
+-- User settings
+
+vim.opt.number = true
+vim.opt.relativenumber = true
